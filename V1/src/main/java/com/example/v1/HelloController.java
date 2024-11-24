57629e05ed4ad43eabe07e05b671cfab9437ae48
BOKA4K <taherkasheek2000@gmail.com>
2024-11-24 09:31:52 +0200

    v1

---
+++ V1/src/main/java/com/example/v1/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.v1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to JavaFX Application!");
+
+        welcomeText.setText("Welcome to JavaFX Application!");
+
+
+    }
+}
